bdb68a25549ef70dea94620f1ff4ad65462d2277
buentellopc <buentellopc@gmail.com>
2022-09-08 22:45:44 -0500

    Add static navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import AllUsers from "./components/pages/AllUsers/AllUsers";
 import NewUser from "./components/pages/NewUser/NewUser";
+import MainNavigation from "./components/layout/MainNavigation/MainNavigation";
 
 function App() {
   return (
     <div className="App">
-      Hey hey
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllUsers />} />
         <Route path="/new-user" element={<NewUser />} />
--- src/components/layout/MainNavigation/MainNavigation.js
+++ src/components/layout/MainNavigation/MainNavigation.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './MainNavigation.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./MainNavigation.module.css";
+import { Link } from "react-router-dom";
 
 const MainNavigation = () => (
-  <div className={styles.MainNavigation}>
-    MainNavigation Component
-  </div>
+  <header className={styles.header}>
+    <div className={styles.logo}>User COE</div>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">All users</Link>
+        </li>
+        <li>
+          <Link to="/new-user">New user</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 MainNavigation.propTypes = {};
