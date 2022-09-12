cf26d1218b594234afe4cd3b68e0d199addd1e87
buentellopc <buentellopc@gmail.com>
2022-09-12 16:17:28 -0500

    Add parent route for user

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import AllUsers from "./components/pages/AllUsers/AllUsers";
 import NewUser from "./components/pages/NewUser/NewUser";
 import Layout from "./components/layout/Layout";
+import TestComponent from "./components/TestComponent";
+import MainUserPage from "./components/pages/MainUserPage";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<AllUsers />} />
-          <Route path="/new-user" element={<NewUser />} />
+          <Route path="/user" element={<MainUserPage />}>
+            <Route index element={<AllUsers />} />
+            <Route path="new-user" element={<NewUser />} />
+          </Route>
+          <Route path="/userInfo">
+            <Route index element={<TestComponent />} />
+          </Route>
         </Routes>
       </Layout>
     </div>
